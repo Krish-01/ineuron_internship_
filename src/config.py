68c69403c7d097d01f30f51f6afdc2f086a7a17f
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-07-26 17:47:41 +0530

    Get MONOGO_DB_URL from st.scerets

--- src/config.py
+++ src/config.py
@@ -1,12 +1,18 @@
-import pymongo
-import pandas as pd
-import json
 import os
 from dataclasses import dataclass
+
+import pymongo
+import streamlit as st
+
 
 @dataclass
 class EnvironmentVariable:
     mongo_db_url = os.getenv("MONGO_DB_URL")
+
+    def __post_init__(self):
+        if self.mongo_db_url is None:
+            self.mongo_db_url = st.secrets['MONGO_DB_URL']
+
 
 env_var = EnvironmentVariable()
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
